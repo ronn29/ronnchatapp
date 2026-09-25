09980691a0b0f82a82f49db29b8a5629e298e5a2
ronn29 <ronsiddayao9@gmail.com>
2026-09-25 19:28:34 +0800

    Added PWA

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -968,3 +968,21 @@ themeToggle.addEventListener("click", () => {
         ? "☀️"
         : "🌙";
 });
+
+
+// pwa service worker
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/service-worker.js")
+            .then(() => {
+                console.log("Service worker registered.");
+            })
+            .catch((error) => {
+                console.error(
+                    "Service worker registration failed:",
+                    error
+                );
+            });
+    });
+}
